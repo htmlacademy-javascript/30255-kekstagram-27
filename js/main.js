2ba404141ff31bf75ace198c9c6b7ac1716b0aad
Andrusha645 <truth20052006@yandex.ru>
2022-10-06 17:42:54 +0300

    задали стрелочную функцию

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-function getRandomPositiveInteger(a,b) {
+const getRandomPositiveInteger = (a,b) => {
   if (a < 0 || b < 0) {
     return NaN;
   }
@@ -6,7 +6,7 @@ function getRandomPositiveInteger(a,b) {
   const upper = Math.floor(Math.max(a, b));
   const result = Math.random() * (upper - lower + 1) + lower;
   return Math.floor(result);
-}
+};
 const commentLength = (str,maxLength) => str.length <= maxLength;
 getRandomPositiveInteger(1,0);
 commentLength('',140);
