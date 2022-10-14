833b5841e2a0c155e970519b03524c6cd9b4fd71
Andrusha645 <truth20052006@yandex.ru>
2022-10-14 19:39:03 +0300

    вторая проверка дз

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-//getRandomPositiveInteger -функция которое выбирает случайное целое число
+//getRandomPositiveInteger -функция, которое выбирает случайное целое число
 //getRandomArrayElement - функция, которая выбирает случайный элемент из массива
-//makeArray - функция которая создает  массив из 25 сгенерированных объектов с описанием фотографий
-//makeCommentsObj - фнукция, которая генерит один случайный комментарий
-//commentsArray - массив, который наполняется функцией makeCommentsObj в зависимости от его длины, которая так же может генериться функцией getRandomPositiveInteger
+//makePhoto - функция, которая создает  1 сгенерированный объект с описанием фотографий
+//makePhotos - функция, которая создает  массив из 25 сгенерированных объектов с описанием фотографий
+//makeComment - фнукция, которая генерит один случайный комментарий
+//makeComments - функция, которая собирает массив с помощью функции makeComment
+//makeMessage - функция собирает массив случайной длины, полученной из ее параметра, функцией взятия случайного элемента из массива MESSAGE_ARRAY. Затем выполняет склейку этого массива.
 //MESSAGE_ARRAY - массив сообщений
 //MESSAGE_ARRAY-массив имен
+//DESCRIPTION_ARRAY -массив описаний для фотографий
 
+const NUMBER_OF_PHOTOS = 25;
+const DESCRIPTION_ARRAY = ['Море','Пейзаж','Селфи','Природа','Путешествие','Портрет','Коллаж'];
 const NAMES_ARRAY = ['Петя','Сергей','Маша','Влада','Мирон','Аня'];
 const MESSAGE_ARRAY = [
   'Всё отлично!',
@@ -25,27 +30,21 @@ const getRandomPositiveInteger = (a,b) => {
   return Math.floor(result);
 };
 const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
-const makeCommentsObj = () => ({
-  id: getRandomPositiveInteger(0,200),
+const makeMessage = (len) => Array.from({length: len}, () => getRandomArrayElement(MESSAGE_ARRAY)).join(' ');
+const makeComment = (id) => ({
+  id: id,
   avatar: `img/avatar-${ getRandomPositiveInteger(1,6) }.svg`,
-  message: getRandomArrayElement(MESSAGE_ARRAY),
+  message: makeMessage(getRandomPositiveInteger(1,2)),
   name: getRandomArrayElement(NAMES_ARRAY)
 });
-const makeArray = () => {
-  const array = [];
-  for (let i = 1; i <= 25; i++) {
-    const commentsArray = Array.from({length: getRandomPositiveInteger(1,3)}, makeCommentsObj);
-    const newElement = {
-      id: i,
-      url: `photos/${ i }.jpg`,
-      description: 'строка — описание фотографии. Описание придумайте самостоятельно',
-      likes:getRandomPositiveInteger(15,200),
-      comments: commentsArray
-    };
-    array.push(newElement);
-  }
-  return array;
-};
-makeArray();
+const makeComments = (num) => Array.from({length: num}, (_, index) => makeComment(index + 1));
+const makePhoto = (id) => ({
+  id: id,
+  url: `photos/${ id }.jpg`,
+  description: getRandomArrayElement(DESCRIPTION_ARRAY),
+  likes:getRandomPositiveInteger(15,200),
+  comments: makeComments(getRandomPositiveInteger(1,3))
+});
+const makePhotos = () => Array.from({length: NUMBER_OF_PHOTOS }, (_, index) => makePhoto(index + 1));
+makePhotos();
 //const commentLength = (str,maxLength) => str.length <= maxLength;
-
