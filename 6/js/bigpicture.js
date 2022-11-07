0fbd7496850f39455dfbafe980dcb727a6d1b34b
Keks <keks@htmlacademy.ru>
2022-11-07 19:51:19 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/bigpicture.js
@@ -0,0 +1,71 @@
+const bigPicture = document.querySelector('.big-picture');
+const commentsCount = document.querySelector('.social__comment-count');
+const commentList = document.querySelector('.social__comments');
+const commentsLoader = document.querySelector('.comments-loader');
+const body = document.querySelector('body');
+const cancelButton = document.querySelector('.big-picture__cancel');
+
+const createComment = ({avatar, name, message}) => {
+  const comment = document.createElement('li');
+  comment.innerHTML =
+'<img class="social__picture" src="" alt="" width="35" height="35"><p class="social__text"></p>';
+  comment.classList.add('.social__comment');
+
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+
+
+const renderComments = (comments) => {
+  commentList.innerHTML = '';
+
+  const fragment = document.createDocumentFragment();
+  comments.forEach((comment) => {
+    const commentElement = createComment(comment);
+    fragment.append(commentElement);
+  });
+
+  commentList.append(fragment);
+};
+
+const hideBigPicture = () => {
+  bigPicture.classList.add('hidden');
+  body.classList.remove('modal-open');
+};
+
+function onEscKeyDown(evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    hideBigPicture();
+  }
+}
+
+const onCancelButtonClick = () => {
+  hideBigPicture();
+};
+
+const renderPicture = ({ url, likes, description }) => {
+  bigPicture.querySelector('.big-picture__img img').src = url;
+  bigPicture.querySelector('.big-picture__img img').alt = description;
+  bigPicture.querySelector('.likes-count').textContent = likes;
+  bigPicture.querySelector('.social__caption').textContent = description;
+
+};
+
+const showBigPicture = (data) => {
+  bigPicture.classList.remove('hidden');
+  body.classList.add('modal-open');
+  commentsLoader.classList.add('hidden');
+  commentsCount.classList.add('hidden');
+  document.addEventListener('keydown', onEscKeyDown);
+
+  renderPicture(data);
+  renderComments(data.comments);
+};
+
+cancelButton.addEventListener('click', onCancelButtonClick);
+
+export{showBigPicture};
---
+++ 6/js/main.js
@@ -0,0 +1,4 @@
+import {makePhotos} from './data.js';
+import {renderPictures} from './picture.js';
+
+renderPictures(makePhotos());
